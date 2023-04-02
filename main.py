8d567bc12d6788cef9e6dde552570470a4337ca2
mringeling17 <matias.ringeling@mail.udp.cl>
2023-04-02 18:35:27 -0400

    invoke function from python

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import asyncio
 from supabase import create_client
 from dotenv import load_dotenv
 load_dotenv()
@@ -14,6 +15,9 @@ supabase = create_client(url, key)
 COLUMNS = ["Doc. Date", "FORETV\n Doc. #", "Business Partner", "Agency", "Sales \nExec.", "Product", "Order", "Total Foreing Currency"]
 FOLDER = "invoice/"
 
+CHANNEL = "A&E"
+TIPO_VENTA = "ON AIR"
+
 def get_invoice_data(file):
     try:
         df = pd.read_excel(FOLDER+file, sheet_name="Sheet1")
@@ -24,6 +28,8 @@ def get_invoice_data(file):
 def filter_columns(df, columns):
     try:
         df = df[columns]
+
+
         return df
     except Exception as e:
         print(e)
@@ -35,21 +41,34 @@ def get_csv(df, file):
         print(e)
 
 def upload_file(file):
-    print(supabase)
-    print('///')
     try:
-        
-        # res = supabase.storage.create_bucket('invoice')
-
-        res = supabase.storage.get_bucket('invoice-dev')
-
-        res = supabase.storage.from_('invoice-dev').upload('/invoices/invoice.csv', FOLDER+file+".csv")
-        print(res)
+       supabase.storage.from_('invoice-dev').upload('/invoices/invoice.csv', FOLDER+file+".csv")
     except Exception as e:
         print(e)
     
-    
+def add_channel(df, channel):
+    try:
+        df["Channel"] = channel
+        return df
+    except Exception as e:
+        print(e)
 
+def add_tipoVenta(df, tipoVenta):
+    try:
+        df["Tipo Venta"] = tipoVenta
+        return df
+    except Exception as e:
+        print(e)
+
+
+async def invoke_function(loop):
+    try:
+        res = await supabase.functions().invoke('invoice_ETL', invoke_options={'body':{}})
+        
+        print(res)
+        return res
+    except Exception as e:
+        print(e)
 
 def main():
     invoice_data = get_invoice_data("invoice.xls")
@@ -58,8 +77,15 @@ def main():
     invoice_data = filter_columns(invoice_data, COLUMNS)
     invoice_data = invoice_data.dropna()
 
+    invoice_data = add_channel(invoice_data, CHANNEL)
+    invoice_data = add_tipoVenta(invoice_data, TIPO_VENTA)
+
     get_csv(invoice_data, "invoice")
     upload_file("invoice")
+
+    loop = asyncio.get_event_loop()
+    resp = loop.run_until_complete(invoke_function(loop))
+    loop.close()
     
 
 
